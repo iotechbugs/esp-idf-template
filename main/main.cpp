43bc7d557eafa15258d2aedc088f1f2e114d4e7f
Sankar <sankar.boopathi@team.telstra.com>
2021-02-12 08:26:19 +0530

    Updating template with default arduino code

--- main/main.cpp
+++ main/main.cpp
@@ -6,39 +6,19 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 
-#include "OneButton.h"
-
-#define PIN_INPUT 23
-
-
+// To enable CPP support
 extern "C" {
 void app_main(void);
 }
 
-void click()
-{
-  Serial.println("Clicked ONCE");
-}
 
-void doubleClick()
-{
-  Serial.println("Clicked TWICE");
-}
-
-void longPress(){
-  Serial.println("Long PRESS detected");
-}
-
+// Main function or entry point
 void app_main() {
 	initArduino();
 	Serial.begin(115200);
-	OneButton button(PIN_INPUT, true);
-	button.setPressTicks(5000);
-	button.attachClick(click);
-	button.attachDoubleClick(doubleClick);
-	button.attachLongPressStart(longPress);
 	while(true){
-		button.tick();
-		delay(10);
+		// Similar arduino loop
+		Serial.println("Test");
+		delay(1000);
 	}
 }
